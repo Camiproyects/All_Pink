5bfb458576872d2f8acda9f695efa3d5d9d0adae
Camiproyects <andresillopillo2004@gmail.com>
2024-08-31 11:25:19 -0500

    Actualizaci├│n autom├ítica: sáb. 31/08/2024 11:25:18,99

---
+++ AU.bat
@@ -0,0 +1,18 @@
+@echo off
+:: Define tu mensaje de commit o usa uno predeterminado
+set commit_message="Actualización automática: %date% %time%"
+
+:: Agrega todos los cambios
+git add .
+
+:: Confirma los cambios
+git commit -m %commit_message%
+
+:: Sube los cambios al repositorio remoto
+git push origin main
+
+:: Opción para un branch específico
+:: git push origin tu_rama
+
+echo Actualización subida exitosamente.
+pause
