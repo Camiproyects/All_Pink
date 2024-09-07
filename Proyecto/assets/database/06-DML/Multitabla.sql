80210ad8059e846d5ebb3de3e8af767fb4d6045f
Camiproyects <andresillopillo2004@gmail.com>
2024-09-07 08:51:40 -0500

    Actualizaci├│n autom├ítica: sáb. 07/09/2024  8:51:40,03

--- Proyecto/assets/database/06-DML/Multitabla.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    r.Nombre AS Rol,
-    
-FROM tienda_ropa.rol AS r
-INNER JOIN tienda_ropa.cliente AS cl
-    ON r.Codigo = cl.RolID
-INNER JOIN tienda_ropa.empleado AS e
-    ON r.Codigo = e.EmpleadoID;
